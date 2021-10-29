54838992ea25bb12a644bb4fdbaa85d23c6650b1
jasonGhostDev <hyotiger777@gmail.com>
2021-10-30 04:02:51 +1000

    update package.json

---
+++ .next/server/chunks/85.js
@@ -0,0 +1,94 @@
+exports.id = 85;
+exports.ids = [85];
+exports.modules = {
+
+/***/ 866:
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+"use strict";
+/* harmony export */ __webpack_require__.d(__webpack_exports__, {
+/* harmony export */   "Z": () => (__WEBPACK_DEFAULT_EXPORT__)
+/* harmony export */ });
+/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(297);
+/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(react__WEBPACK_IMPORTED_MODULE_0__);
+/* harmony import */ var next_link__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(664);
+/* harmony import */ var _styles_patterns_footer_module_css__WEBPACK_IMPORTED_MODULE_3__ = __webpack_require__(205);
+/* harmony import */ var _styles_patterns_footer_module_css__WEBPACK_IMPORTED_MODULE_3___default = /*#__PURE__*/__webpack_require__.n(_styles_patterns_footer_module_css__WEBPACK_IMPORTED_MODULE_3__);
+/* harmony import */ var react_jsx_runtime__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(282);
+/* harmony import */ var react_jsx_runtime__WEBPACK_IMPORTED_MODULE_2___default = /*#__PURE__*/__webpack_require__.n(react_jsx_runtime__WEBPACK_IMPORTED_MODULE_2__);
+
+
+
+
+
+
+const Footer = () => {
+  return /*#__PURE__*/react_jsx_runtime__WEBPACK_IMPORTED_MODULE_2__.jsx("footer", {
+    className: (_styles_patterns_footer_module_css__WEBPACK_IMPORTED_MODULE_3___default().footer),
+    children: /*#__PURE__*/(0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_2__.jsxs)("div", {
+      className: (_styles_patterns_footer_module_css__WEBPACK_IMPORTED_MODULE_3___default().flexGap),
+      children: [/*#__PURE__*/react_jsx_runtime__WEBPACK_IMPORTED_MODULE_2__.jsx("p", {
+        className: "text_sec_14",
+        children: "Proudly powered by Whale Marker NFT Pools\xA9 2021. All rights reserved."
+      }), /*#__PURE__*/(0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_2__.jsxs)("div", {
+        className: (_styles_patterns_footer_module_css__WEBPACK_IMPORTED_MODULE_3___default().flex),
+        children: [/*#__PURE__*/react_jsx_runtime__WEBPACK_IMPORTED_MODULE_2__.jsx(next_link__WEBPACK_IMPORTED_MODULE_1__.default, {
+          href: "/privacy_policy",
+          children: "Privacy Policy"
+        }), /*#__PURE__*/react_jsx_runtime__WEBPACK_IMPORTED_MODULE_2__.jsx("a", {
+          href: "/",
+          target: "_blank",
+          rel: "noopener noreferrer",
+          children: /*#__PURE__*/react_jsx_runtime__WEBPACK_IMPORTED_MODULE_2__.jsx("img", {
+            src: "/icons/github.svg",
+            alt: "github"
+          })
+        }), /*#__PURE__*/react_jsx_runtime__WEBPACK_IMPORTED_MODULE_2__.jsx("a", {
+          href: "/",
+          target: "_blank",
+          rel: "noopener noreferrer",
+          children: /*#__PURE__*/react_jsx_runtime__WEBPACK_IMPORTED_MODULE_2__.jsx("img", {
+            src: "/icons/twitter.svg",
+            alt: "twiiter"
+          })
+        }), /*#__PURE__*/react_jsx_runtime__WEBPACK_IMPORTED_MODULE_2__.jsx("a", {
+          href: "https://discord.gg/Npfz78G5",
+          target: "_blank",
+          rel: "noopener noreferrer",
+          children: /*#__PURE__*/react_jsx_runtime__WEBPACK_IMPORTED_MODULE_2__.jsx("img", {
+            src: "/icons/discord.svg",
+            alt: "discord"
+          })
+        })]
+      })]
+    })
+  });
+};
+
+/* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = (Footer);
+
+/***/ }),
+
+/***/ 205:
+/***/ ((module) => {
+
+// Exports
+module.exports = {
+	"footer": "footer_footer__1LtCf",
+	"content": "footer_content__3_C4D",
+	"flexGap": "footer_flexGap__Kd0CQ",
+	"flex": "footer_flex__1KKGD"
+};
+
+
+/***/ }),
+
+/***/ 431:
+/***/ (() => {
+
+/* (ignored) */
+
+/***/ })
+
+};
+;
