a58578ccdbc4167d6400e1afddd0c6b5ce852484
CHM <chm111119951124@gmail.com>
2021-10-30 09:17:01 +0900

    update: from next to react

--- src/app/patterns/hero.jsx
+++ src/app/patterns/hero.jsx
@@ -12,7 +12,7 @@ const Hero = () => {
 
     useEffect(() => {
         loadWallet().then();
-    },[])
+    })
 
     const loadWallet = async () => {
         if(getLocalStorageWalletAddress() !== '') {
